91a855703eb1e3647865f3cb7aff144b1113cd42
halu5071 <horiiortho5@gmail.com>
2019-03-08 19:33:00 +0900

    initial commit

---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef LIBPOINT_LIBRARY_H
+#define LIBPOINT_LIBRARY_H
+
+void hello();
+
+#endif
---
+++ test/Tests.cpp
@@ -0,0 +1,9 @@
+/*
+ * Created by Yasunori Horii on 2019/03/08.
+ */
+
+#include <gtest/gtest.h>
+
+TEST(unitTests, test1) {
+  EXPECT_EQ(1, 1);
+}
