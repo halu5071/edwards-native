57f39ba89ba5ade7d064940d1f41f27dd4374c11
Yasunori Horii <horiiortho5@gmail.com>
2019-03-21 16:02:55 +0900

    Add extern C statement

--- library.h
+++ library.h
@@ -3,6 +3,14 @@
 
 #include <gmpxx.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void scalar_mul(mpz_ptr dest_x, mpz_ptr dest_y, mpz_srcptr point_x, mpz_srcptr point_y, mpz_srcptr seed);
+
+#ifdef  __cplusplus
+};
+#endif
 
 #endif // LIBPOINT_LIBRARY_H
